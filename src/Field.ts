f8f9f571ea62ecbe1b6b3e47165f6f67a3acb105
Alan Marcell <alanmarcell@live.com>
2017-09-22 00:38:44 -0300

    Init terminal

--- src/Field.ts
+++ src/Field.ts
@@ -1,115 +1,7 @@
-import R from 'ramda';
-import { IField, IFieldConfig } from './IField';
-import { IPosition, IPositionArgs } from './IPosition';
-
-/**
- * Checks if field has position.
- */
-const positionIsValid = R.curry((field: IField, p: IPositionArgs) => {
-    return p.x >= 0 && p.x < field.length && p.y >= 0 && p.y < field[0].length;
-});
-
-/**
- * Receives a pos and return his near positions
- * args {IPositionArgs}
- * returns {IPositionArgs[]}
- */
-const nearPositions = (pos: IPositionArgs) => {
-    const range = R.range(-1, 2);
-    /**
-     * Get a 3x3 position array with the position and all the near positions then remove the position itseft
-     */
-    return R.flatten(R.remove(4, 1, range.map(i => range.map(j => {
-        return { x: pos.x + i, y: pos.y + j };
-    }))));
-};
-
-const validNearPos = R.curry((field: IField, pos: IPositionArgs) =>
-    R.filter(positionIsValid(field), nearPositions(pos)));
-
-const openPosition = (pos: IPosition) => {
-    const openedPos = pos;
-    openedPos.opened = true;
-    return openedPos;
-};
-
-const markPosition = (pos: IPosition) => {
-    const markedPos = updatePos(pos);
-    if (markedPos.marked === 2) markedPos.marked = 0;
-    else markedPos.marked++;
-    return markedPos;
-};
-
-const isValidConfig = (fieldConfig: IFieldConfig) => {
-    const totalPositions = fieldConfig.width * fieldConfig.height;
-    return totalPositions > fieldConfig.bombs ? true : false;
-};
-
-const allPositions = (field: IField) => field.reduce((a, b) => a.concat(b));
-
-const countNearBombs = (field: IField) => {
-    const countedField: IField = field;
-    allPositions(field).map(pos => {
-        if (pos.isBomb) validNearPos(field, pos).map(p => countedField[p.x][p.y].nearBombs++);
-    }); // TODO immutable
-    return countedField;
-};
-
-// TODO use ptz-math and help with any math method you need
-const getRandomPos = (field: IField, fieldConfig: IFieldConfig) => {
-    const width = Math.floor((fieldConfig.width - 1) * Math.random() + 1);
-    const height = Math.floor((fieldConfig.height - 1) * Math.random() + 1);
-    return field[width][height];
-};
-
-const bombPos = (field: IField, config: IFieldConfig) => {
-    const pos = getRandomPos(field, config);
-    if (pos.isBomb) {
-        return bombPos(field, config);
-    }
-    pos.isBomb = true;
-    return field;
-};
-
-/**
- * Populate new field with bombs
- */
-const getBombedField = (field: IField, config: IFieldConfig) => {
-    const fieldToBomb = R.clone(field);
-    return R.last(R.range(0, config.bombs).map(() => bombPos(fieldToBomb, config)));
-};
-
-const getEmptyField = (fieldConfig: IFieldConfig) => {
-    const widthRange = R.range(0, fieldConfig.width);
-    const heightRange = R.range(0, fieldConfig.height);
-
-    return widthRange.map(i => heightRange.map(j => newPos(i, j)));
-};
-
-/**
- * Get a new position
- */
-const newPos = (x: number, y: number) => {
-    return {
-        x, y, isBomb: false, nearBombs: 0,
-        opened: false, marked: 0, isValid: true
-    };
-};
-
-const updatePos = (pos: IPosition) => R.clone(pos);
-
-const getInitialField = (fieldConfig: IFieldConfig) => {
-    if (!isValidConfig(fieldConfig)) throw new Error('Invalid field configuration');
-
-    const emptyField: IField = getEmptyField(fieldConfig);
-
-    const bombedField: IField = getBombedField(emptyField, fieldConfig);
-
-    return countNearBombs(bombedField);
-};
+import { IField } from 'minesweeper-core';
 
 // TODO break in small functions
-function logField(field): void {
+function logField(field, clicked: { x, y }): void {
     const countedField: IField = field;
     const indexColor = '\x1b[37m';
     const resetColor = '\x1b[0m';
@@ -131,31 +23,34 @@ function logField(field): void {
                     row += '---';
                 } else {
                     const numBombs = countedField[pos.x][pos.y].nearBombs;
-                    let numBombsString;
+                    let wasClicked;
+                    if (pos.x === clicked.x && pos.y === clicked.y)
+                        wasClicked = true;
+                    let numBombsString = wasClicked ? '\x1b[47m' : '';
                     switch (numBombs) {
                         case 1:
-                            numBombsString = '\x1b[34m' + numBombs;
+                            numBombsString += '\x1b[34m' + numBombs;
                             break;
                         case 2:
-                            numBombsString = '\x1b[32m' + numBombs;
+                            numBombsString += '\x1b[32m' + numBombs;
                             break;
                         case 3:
-                            numBombsString = '\x1b[33m' + numBombs;
+                            numBombsString += '\x1b[33m' + numBombs;
                             break;
                         case 4:
-                            numBombsString = '\x1b[35m' + numBombs;
+                            numBombsString += '\x1b[35m' + numBombs;
                             break;
                         case 5:
-                            numBombsString = '\x1b[36m' + numBombs;
+                            numBombsString += '\x1b[36m' + numBombs;
                             break;
                         case 7:
-                            numBombsString = '\x1b[31m' + numBombs;
+                            numBombsString += '\x1b[31m' + numBombs;
                             break;
                         case 8:
-                            numBombsString = '\x1b[37m' + numBombs;
+                            numBombsString += '\x1b[37m' + numBombs;
                             break;
                         default:
-                            numBombsString = '\x1b[37m' + numBombs;
+                            numBombsString += '\x1b[37m' + numBombs;
                     }
 
                     line += ' ' + numBombsString + resetColor + ' ';
@@ -175,17 +70,5 @@ function logField(field): void {
 }
 
 export {
-    allPositions,
-    getInitialField,
-    getEmptyField,
-    getBombedField,
-    countNearBombs,
-    logField,
-    markPosition,
-    nearPositions,
-    newPos,
-    openPosition,
-    positionIsValid,
-    validNearPos,
-    updatePos
+    logField
 };
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
-import { clickPosition, IFieldConfig, startBattle } from 'minesweeper-core';
+import { battleMarkPosition, clickPosition, IFieldConfig, startBattle } from 'minesweeper-core';
+import * as readline from 'readline';
 import { logField } from './Field';
 
 const fieldConfig: IFieldConfig = {
@@ -8,14 +9,40 @@ const fieldConfig: IFieldConfig = {
 };
 
 var battle = startBattle(fieldConfig);
-do {
-    const x = Math.floor((battle.field.length - 1) * Math.random() + 1);
-    const y = Math.floor((battle.field[0].length - 1) * Math.random() + 1);
-
-    battle = clickPosition(battle, { x, y });
-    logField(battle.field);
+// do {
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+let x, y;
+play();
+function play() {
+    rl.question('Chose vertical col: ', (answer) => {
+        // TODO: Log the answer in a database
+        console.log('you choose:', answer);
+        x = +answer - 1;
+        // rl.close();
+        rl.question('Chose horizontal pos ', (a) => {
+            rl.question('Chose mark (m) or open (o)  pos ', (m) => {
+                // TODO: Log the answer in a database
+                console.log('you choose:', a);
+                y = +a - 1;
+                battle = m === 'o' ? clickPosition(battle, { x, y }) : battleMarkPosition(battle, { x, y });
+                logField(battle.field, { x, y });
+                if (!battle.isOver) {
+                    play();
+                } else {
+                    rl.close();
+                }
+            });
+        });
+    });
 }
-while (!battle.isOver);
+// const x = Math.floor((battle.field.length - 1) * Math.random() + 1);
+// const y = Math.floor((battle.field[0].length - 1) * Math.random() + 1);
+
+// }
+// while (!battle.isOver);
 
 export {
     clickPosition, startBattle, IFieldConfig
